fc2f105f713fc9b2e3218062b547551b8f433409
eoFranca <erfranca.com@gmail.com>
2023-05-09 11:56:51 -0300

    OK

--- src/App.vue
+++ src/App.vue
@@ -66,31 +66,43 @@ const produtos = ref([
 
 //---------------------------------------------------------------
 
-const carrinho = ref([
-    
-])
+const carrinho = ref({
+    items:[],
+    total: 0,
+})
 
 function incrementar(index) {
     // carrinho.value[index].quantidade++
-    carrinho.value[index].valorTotal = carrinho.value[index].preco * ++carrinho.value[index].quantidade
+    carrinho.value.total -= carrinho.value.items[index].valorTotal
+    carrinho.value.items[index].valorTotal = carrinho.value.items[index].preco * ++carrinho.value.items[index].quantidade
+    carrinho.value.total += carrinho.value.items[index].valorTotal
 }
 function decrementar(index) {
-    if (carrinho.value[index].quantidade > 1) {
-        carrinho.value[index].valorTotal = carrinho.value[index].preco * --carrinho.value[index].quantidade
+    if (carrinho.value.items[index].quantidade > 1) {
+        carrinho.value.total -= carrinho.value.items[index].valorTotal
+        carrinho.value.items[index].valorTotal = carrinho.value.items[index].preco * --carrinho.value.items[index].quantidade
+        carrinho.value.total += carrinho.value.items[index].valorTotal
     }
 }
 function remover(index){
-    carrinho.value.splice(index,1)
+    carrinho.value.total =  carrinho.value.total - carrinho.value.items[index].valorTotal
+    carrinho.value.items.splice(index,1)
+
 }
 
 function addToCart(index) {
-    carrinho.value.push({
+    carrinho.value.items.push({
         id: carrinho.value.length + 1,
         nome: produtos.value[index].nome,
         preco: produtos.value[index].preco,
         quantidade: 1,
         valorTotal: produtos.value[index].preco * 1
     })
+    carrinho.value.total += produtos.value[index].preco * 1
+}
+let valorTotal = ref('')
+function valorTtl ( ){
+valorTotal = carrinho.value[index].preco + carrinho.value[index].preco
 }
 </script>
 
@@ -101,7 +113,7 @@ function addToCart(index) {
     <ul>
         <h1>Produtos</h1>
         <li v-for="(item, index) in produtos ">
-            {{ item.nome }} - R${{ item.preco }} <button @click="addToCart(index)">-></button>
+            {{ item.nome }} - R${{ item.preco }} <button @click="addToCart(index)">add</button>
 
         </li>
     </ul>
@@ -109,17 +121,41 @@ function addToCart(index) {
     <div class="carrinho">
         <ul>
             <h1>Carrinho</h1>
-            <li v-for="(item, index) in carrinho"> {{ item.nome }} - {{ item.valorTotal.toFixed(2) }}
+            <li v-for="(item, index) in carrinho.items"> {{ item.nome }} - {{ item.valorTotal.toFixed(2) }}
                 <button @click="incrementar(index)">+</button>
                 {{ item.quantidade }}
                 <button @click="decrementar(index)">-</button>
-                <button @click="remover">Remover</button>
+                <button @click="remover(index)">Remover</button>
             </li>
         </ul>
+     <div class="valortotal">
+         <h4>valor Total:{{ carrinho.total }}</h4>
+    </div>
+        
     </div>
 </template>     
 
 
-<style scoped></style>
+<style scoped>
+
+li{
+    padding: 20%;
+    border: 1px solid black;
+    list-style: none;
+    display: grid;
+    width: 250px;
+    margin-top: 10px;
+    background-color: floralwhite;
+    
+}
+.valortotal{
+    padding: 5%;
+    border: 1px solid black;
+    margin-left: 40px;
+    margin-top: 10px;
+    background-color: floralwhite;
+   
+}
+</style>
 
 
